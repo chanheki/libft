676463a733162c6cc01bfc2164fd0da90fa024cc
chanhihi <chanhihi55@gmail.com>
2022-08-20 03:27:05 +0900

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -10,14 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	if (!lst || !del)
 		return;
-	if ((*lst)-> next)
+	if ((*lst) -> next)
 		ft_lstclear(&((*lst)-> next), del);
 	ft_lstdelone(*lst, del);
 	*lst = NULL;
