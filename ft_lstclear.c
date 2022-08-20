00875f5bb7f4f10c6031e95ba2c5ed9eec2dd66d
chanhihi <chanhihi55@gmail.com>
2022-08-21 03:20:21 +0900

    Create ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 12:12:39 by chanheki          #+#    #+#             */
+/*   Updated: 2022/07/12 18:56:44 by chanheki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return;
+	if ((*lst)-> next)
+		ft_lstclear(&((*lst)-> next), del);
+	ft_lstdelone(*lst, del);
+	*lst = NULL;
+}
