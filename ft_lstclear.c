288272b3dde119b0c702d1cb448c0eea2c8b819b
chanhihi <chanhihi55@gmail.com>
2022-08-28 03:55:05 +0900

    Update ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -15,8 +15,8 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	if (!lst || !del)
-		return;
-	if ((*lst) -> next)
+		return ;
+	if ((*lst)-> next)
 		ft_lstclear(&((*lst)-> next), del);
 	ft_lstdelone(*lst, del);
 	*lst = NULL;
